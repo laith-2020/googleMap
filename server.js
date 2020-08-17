bf4e99b360b571613761ca03016f26362a192308
laith <mr.layth_2011@hotmail.com>
2020-08-17 11:42:18 +0300

    add how to use

--- server.js
+++ server.js
@@ -10,18 +10,19 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// how to use:
+// 1- create file with name (.env)
+// 2- copy the following inside the .env file 
 
-// app.get("/", (req, res) => {
-//     let api_key_public = process.env.PUBLIC_KEY;
-//     let venue_id = process.env.VENUE_ID;
-//     // https://besttime.app/api/v1/forecasts/busy?api_key_public=pub_56fa2489345347d19af7abe5284a7f43&venue_id=ven_3055525142546652696c5752346a4c6c625838357151704a496843
-//     let url = `https://besttime.app/api/v1/forecasts/busy?api_key_public=${api_key_public}&venue_id=${venue_id}`;
-//     superagent.get(url).then((result) => {
-//         let busyObj = new Busy(result.body);
-//         // console.log(result.body);
-//         res.send("index");
-//     });
-// });
+//  PORT=3030
+//  PUBLIC_KEY=pub_56fa2489345347d19af7abe5284a7f43
+//  VENUE_ID=ven_3055525142546652696c5752346a4c6c625838357151704a496843
+
+// 3- install the packages  in your ubuntu terminal :
+
+// npm i express ejs superagent dotenv 
+
+// finally run your terminal with this command   ( nodemon )
 
 
 app.get('/', (req, res) => {
