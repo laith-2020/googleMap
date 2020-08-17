99ba7cb7c9659ad1155be357daed8425869c302f
laith <mr.layth_2011@hotmail.com>
2020-08-17 11:27:45 +0300

    finish-popular-time-chart

--- public/app.js
+++ public/app.js
@@ -1,47 +1,175 @@
-'use strict';
+"use strict";
 
-// $('#Hello').click(() => {
-//     console.log('clicked');
-// })
+$("#main").hide();
+
+let day_raw = [];
+for (let i = 0; i < 24; i++) {
+    if (i > 2 && i < 16) {
+        day_raw.push($(`.days${i}`).html().trim());
+    }
+}
 
 
-// const days = require('../app.js');
-
-
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+//chart
+var options = {
+    series: [{
+        name: 'besy status',
+        data: day_raw
+    }],
+    chart: {
+        height: 300,
+        width: 500,
+        type: 'bar',
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                position: 'top', // top, center, bottom
+            },
+        }
+    },
+    dataLabels: {
+        enabled: true,
+        formatter: function(val) {
+            return val + "%";
+        },
+        offsetY: -20,
+        style: {
+            fontSize: '12px',
+            colors: ["#304758"]
+        }
+    },
+
+    xaxis: {
+        categories: ["9am",
+            "10am",
+            "11am",
+            "12pm",
+            "1pm",
+            "2pm",
+            "3pm",
+            "4pm",
+            "5pm",
+            "6pm",
+            "7pm",
+            "8pm",
+            "9pm",
+        ],
+
+        position: 'bottom',
+        axisBorder: {
+            show: false
+        },
+        axisTicks: {
+            show: false
+        },
+        crosshairs: {
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    colorFrom: '#D8E3F0',
+                    colorTo: '#BED1E6',
+                    stops: [9, 21],
+                    opacityFrom: 0.4,
+                    opacityTo: 0.5,
                 }
-            }]
+            }
+        },
+        tooltip: {
+            enabled: true,
+        }
+    },
+    yaxis: {
+        axisBorder: {
+            show: false
+        },
+        axisTicks: {
+            show: false,
+        },
+        labels: {
+            show: false,
+            formatter: function(val) {
+                return val + "%";
+            }
+        }
+
+    },
+    title: {
+        text: 'busy hours',
+        floating: true,
+        offsetY: 330,
+        align: 'center',
+        style: {
+            color: '#444'
         }
     }
-});
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+
+// -------------------------------------------------------------------------------
+// var ctx = document.getElementById("myChart").getContext("2d");
+// var myChart = new Chart(ctx, {
+//     type: "bar",
+//     data: {
+//         labels: [
+//             "9am",
+//             "10am",
+//             "11am",
+//             "12pm",
+//             "1pm",
+//             "2pm",
+//             "3pm",
+//             "4pm",
+//             "5pm",
+//             "6pm",
+//             "7pm",
+//             "8pm",
+//             "9pm",
+//         ],
+//         datasets: [{
+//             label: "Busy Status",
+//             data: day_raw,
+//             backgroundColor: [
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//             ],
+//             borderColor: [
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(255, 99, 132, 1)",
+//             ],
+//             borderWidth: 1,
+//         }, ],
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true,
+//                 },
+//             }, ],
+//         },
+//     },
+// });
--- server.js
+++ server.js
@@ -1,93 +1,54 @@
-'use strict';
-
-require('dotenv').config();
-const express = require('express');
-const superagent = require('superagent');
-
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const superagent = require("superagent");
+const { get } = require("superagent");
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(express.static('./public'));
-app.set('view engine', 'ejs');
-
-
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// dotenv 
-// PORT=3030
-// PUBLIC_KEY=pub_56fa2489345347d19af7abe5284a7f43
-// VENUE_ID=ven_3055525142546652696c5752346a4c6c625838357151704a496843
-
-
-// app.get('/', (req, res) => {
-//     res.status(200).send('its work');
-// })
-
-app.get('/', (req, res) => {
-
-    let api_key_public = process.env.PUBLIC_KEY;
-    let venue_id = process.env.VENUE_ID;
-
-    // https://besttime.app/api/v1/forecasts/busy?api_key_public=pub_56fa2489345347d19af7abe5284a7f43&venue_id=ven_3055525142546652696c5752346a4c6c625838357151704a496843
-    let url = `https://besttime.app/api/v1/forecasts/busy?api_key_public=${api_key_public}&venue_id=${venue_id}`;
-
-    superagent.get(url)
-        .then(result => {
-            let busyObj = new Busy(result.body);
-            console.log(result.body);
-            res.render('index', { busyObj: result.body });
-        })
-
-})
-
-app.get('/data', (req, res) => {
-    let api_key_public = process.env.PUBLIC_KEY;
-    let venue_id = process.env.VENUE_ID;
-    // https: //besttime.app/api/v1/forecasts/day?api_key_public=pub_56fa2489345347d19af7abe5284a7f43&venue_id=ven_3055525142546652696c5752346a4c6c625838357151704a496843
-    let url = `https://besttime.app/api/v1/forecasts/day?api_key_public=${api_key_public}&venue_id=${venue_id}`;
-    superagent.get(url)
-        .then(result => {
-            let busyObj = new Day(result.body);
-            console.log(result.body);
-            res.send(busyObj);
-        })
-})
-
-
-// var days = {};
-
-// days.saja = function() {
+// app.get("/", (req, res) => {
 //     let api_key_public = process.env.PUBLIC_KEY;
 //     let venue_id = process.env.VENUE_ID;
-//     // https: //besttime.app/api/v1/forecasts/day?api_key_public=pub_56fa2489345347d19af7abe5284a7f43&venue_id=ven_3055525142546652696c5752346a4c6c625838357151704a496843
-//     let url = `https://besttime.app/api/v1/forecasts/day?api_key_public=${api_key_public}&venue_id=${venue_id}`;
-//     superagent.get(url)
-//         .then(result => {
-//             let busyObj = new Day(result.body);
-//             return busyObj;
-//         })
+//     // https://besttime.app/api/v1/forecasts/busy?api_key_public=pub_56fa2489345347d19af7abe5284a7f43&venue_id=ven_3055525142546652696c5752346a4c6c625838357151704a496843
+//     let url = `https://besttime.app/api/v1/forecasts/busy?api_key_public=${api_key_public}&venue_id=${venue_id}`;
+//     superagent.get(url).then((result) => {
+//         let busyObj = new Busy(result.body);
+//         // console.log(result.body);
+//         res.send("index");
+//     });
+// });
 
-// }
-// module.exports = days;
 
+app.get('/', (req, res) => {
+    let api_key_public = process.env.PUBLIC_KEY;
+    let venue_id = process.env.VENUE_ID;
+    var d = new Date();
+    var n = d.getDay();
+    console.log(n);
+    // https://besttime.app/api/v1/forecasts/day/raw?api_key_public=pub_56fa2489345347d19af7abe5284a7f43&venue_id=ven_3055525142546652696c5752346a4c6c625838357151704a496843&day_int=1
+    let url = `https://besttime.app/api/v1/forecasts/day/raw?api_key_public=${api_key_public}&venue_id=${venue_id}&day_int=${n}`;
+    superagent.get(url).then((result) => {
+        let finalResult = result.body.analysis.day_raw;
+        console.log(finalResult);
+        let finalArr = finalResult.map((d) => {
+            console.log('d', d);
+            return new Day(d);
+        });
+        res.render("index", { arr: finalArr });
+        console.log(finalArr)
+    })
+})
 
 function Day(busyDay) {
-    this.hour_analysis = busyDay.analysis.hour_analysis;
-
+    this.day_raw = busyDay;
 }
-// api_key_private=pri_95e954eb0bb04265abce88e31ed624bc&
-// venue_name=Falak Business Hub | فلك للأعمال والاستثمار&
-// venue_address=near Invenu, Al Yasmin, Unnamed Road, Riyadh 13326 13326 Saudi Arabia
 
-
-function Busy(busyData) {
-    this.busy_hours = busyData.analysis.busy_hours;
-    this.venue_closed = busyData.analysis.day_info.venue_closed;
-    this.venue_open = busyData.analysis.day_info.venue_open;
-
-}
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
-})
+});
